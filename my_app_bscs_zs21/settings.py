61c548bb857d858490fb789b30652ed224619101
Kris Visaya <kvisaya@gbox.adnu.edu.ph>
2026-05-17 15:48:17 +0800

    .

--- my_app_bscs_zs21/settings.py
+++ my_app_bscs_zs21/settings.py
@@ -75,12 +75,14 @@ WSGI_APPLICATION = 'my_app_bscs_zs21.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test1',
+        'USER': 'member',
+        'PASSWORD': 'member',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
